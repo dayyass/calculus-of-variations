525a042a28422a5a7351634b7d70f41bc097c84d
Dani El-Ayyass <dayyass@yandex.ru>
2021-06-18 15:43:29 +0300

    remove test_all.sh

--- test_all.sh
+++
@@ -1,5 +0,0 @@
-python -m unittest tests/test_simplest_problem.py
-python -m unittest tests/test_boltz_problem.py
-python -m unittest tests/test_isoperimetric_problem.py
-python -m unittest tests/test_higher_derivatives_problem.py
-python -m unittest tests/test_multidimensional_problem.py
