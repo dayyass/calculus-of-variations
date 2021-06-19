d353a02550af2a5b247a525a1973826ea8f4b915
Dani El-Ayyass <dayyass@yandex.ru>
2021-06-19 23:33:18 +0300

    add classes to __init__

--- calculus_of_variations/__init__.py
+++ calculus_of_variations/__init__.py
@@ -1,9 +1,13 @@
 from .boltz_problem import BoltzSolver
+from .higher_derivatives_problem import HigherDerivativesSolver
 from .isoperimetric_problem import IsoperimetricProblemSolver
+from .multidimensional_problem import MultidimensionalSolver
 from .simplest_problem import SimplestProblemSolver
 
 __all__ = [
     "SimplestProblemSolver",
     "BoltzSolver",
     "IsoperimetricProblemSolver",
+    "HigherDerivativesSolver",
+    "MultidimensionalSolver",
 ]
