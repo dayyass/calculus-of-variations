593dc4c225a442e1552f8be7aba780843df704a3
Dani El-Ayyass <dayyass@yandex.ru>
2021-06-22 22:50:10 +0300

    update imports

--- web_interface/multidimensional_problem_dash.py
+++ web_interface/multidimensional_problem_dash.py
@@ -1,8 +1,9 @@
 import sys
 
-from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
+
+from dash import Dash
 from dash.dependencies import Input, Output, State
 
 # TODO: fix it
